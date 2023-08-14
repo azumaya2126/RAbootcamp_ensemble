db573e1cdb03c9c5e85b791fb6033890176beb14
azumaya2126 <azumaya2126@gmail.com>
2023-08-14 10:40:14 +0900

    plot Canada

--- ensemble_code.R
+++ ensemble_code.R
@@ -9,6 +9,7 @@ gdp,
 co2) 
 
 library(tidyverse)
+library(ggplot2)
 
 all_variables <- c("country", "year", "gdp", "co2")
 
@@ -21,18 +22,23 @@ data_2 <- data1 |>
     co2,
     population) |>
   filter(!is.na(country) & !is.na(year)& !is.na(gdp) & !is.na(co2) & !is.na(population)) |>
-  mutate(co2_percap = co2/population,
-         gdp_percap = gdp /population)
-  arrange(desc(year))
+  mutate(co2_percap = co2/population*1000000,
+         gdp_percap = (gdp/1000) /population)
+
 head(data_2)
 
 
-library(ggplot2)
+data_canada <- data_2 |> 
+  filter(country == "Canada")
 
 
-plot1 <- ggplot(data2,
-                aes(x =, y = co2
-                  
-                )
-  
-)
+plot1 <- ggplot(data_canada,
+                aes(x = gdp_percap, y = co2_percap))+
+         geom_line()+
+         labs(x = "GDP per capita ($1000)",
+              y = "CO2 per capita (g)",
+              title = "Canada")
+plot1
+
+data_2 |> filter(country == "China" | country == "United States")
+
